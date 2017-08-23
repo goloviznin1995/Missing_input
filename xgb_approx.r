b2e44ef7aa68b6aea871109bbb4a4a23381a66ae
Anton Goloviznin <goloviznin1995@yandex.ru>
2017-08-24 02:46:10 +0300

    Added paramerer: *add boolean features*

--- xgb_approx.r
+++ xgb_approx.r
@@ -1,5 +1,24 @@
+add_NA_bool_vars <- function(dat, prefix = 'miss_'){
+        for (i in 1:ncol(dat)){
+                if (length(dat[,i][!complete.cases(dat[,i])]) > 0){
+                        name <- colnames(dat)[i]
+                        new_name <- paste(prefix, name, sep = '')
+                        vect <- c(!complete.cases(dat[,i]))
+                        dat[,ncol(dat)+1] <- vect
+                        colnames(dat)[ncol(dat)] <- new_name
+                }
+        }
+        return(dat)
+}
 
-na_approx_xgb <- function(dat, n_rounds = 200, algorithm = 'xgb'){
+na_approx_xgb <- function(dat, 
+                          n_rounds = 200, 
+                          algorithm = 'xgb',
+                          add_na_features = T){
+        if (add_na_features == T){
+                dat <- add_NA_bool_vars(dat)
+                print(ncol(dat))
+        }
         only_missing_msg = 'Variable contains only missing values:'
         m1 = 'Found '
         m3 = '% missing values'
@@ -7,6 +26,7 @@ na_approx_xgb <- function(dat, n_rounds = 200, algorithm = 'xgb'){
         #Verbose 
         
         for (i in 1:ncol(dat)){
+                #print(i)
                 compl <- length(dat[,i][complete.cases(dat[,i])])
                 if (compl == 0){
                         name <- colnames(dat)[i]
@@ -15,6 +35,8 @@ na_approx_xgb <- function(dat, n_rounds = 200, algorithm = 'xgb'){
                 
                 if (compl < nrow(dat) & compl > 0){
                         train <- dat[complete.cases(dat[,i]),]
+                        
+                        prediction <- test[,i]
                         test <- dat[!complete.cases(dat[,i]),]
                         
                         m2 <- as.character(nrow(test)/nrow(dat))
@@ -24,13 +46,20 @@ na_approx_xgb <- function(dat, n_rounds = 200, algorithm = 'xgb'){
                         
                                 
                         train_matrix <- data.matrix(train)
+                        
                         test_matrix <- data.matrix(test)
 
                         xgb <- xgboost(train_matrix, 
                                        label = train[,i], 
                                        nrounds = n_rounds,
                                        verbose = F)
+                        ncol(test)
                         test[,i] <- predict(xgb, newdata = test_matrix)
+                        #test[,ncol(test)+1] <- predict(xgb, newdata = test_matrix)
+                        #test <- data.frame(test[,1:i-1], 
+                        #                  test[,ncol(test)], 
+                        #                  test[,ncol(test) -1])
+                        
                         # Probably add predictions
                         dat <- rbind(test, train) 
                         
